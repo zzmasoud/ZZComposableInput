32ab0f3e7589269f4e0adce12a969505d4ecd576
Masoud Sh <zzmasoud@gmail.com>
2022-09-06 21:31:20 +0200

    Add `ZZHorizontalSelectorViewPresentable` to abstract selectable horizontal items

---
+++ ZZNewTaskView/Models/ZZHorizontalSelectorViewPresentable.swift
@@ -0,0 +1,13 @@
+//
+//  ZZHorizontalSelectorViewPresentable.swift
+//  ZZNewTaskView
+//
+//  Created by Masoud Sheikh Hosseini on 9/6/22.
+//
+
+import Foundation
+
+protocol ZZHorizontalSelectorViewPresentable {
+    var color: CGColor  { get }
+    var title: String { get }
+}
