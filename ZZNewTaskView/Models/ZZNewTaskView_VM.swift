e4e513a020e253d3c84bb20fd800bd1fdc576972
Masoud Sh <zzmasoud@gmail.com>
2022-09-10 19:06:33 +0200

    Add `ZZNewTaskView_VM`

---
+++ ZZNewTaskView/Models/ZZNewTaskView_VM.swift
@@ -0,0 +1,20 @@
+//
+//  ZZNewTaskView_VM.swift
+//  ZZNewTaskView
+//
+//  Created by Masoud Sheikh Hosseini on 9/10/22.
+//
+
+import Foundation
+//import UIKit.UIColor
+
+public protocol ZZNewTaskView_VMP {
+    var isRepeatOptionHidden: Bool { get }
+    func didTapSendButton()
+    var currentTitle: String? { get }
+    var isSendButtonEnabled: Bool { get }
+    func textValueChanged(to text: String?)
+    var selectorViewViewModel: ZZHorizontalSelectorView_VMP { get }
+    func didSelectButton(at index: Int)
+    func setView(delegate: AnyObject)
+}
