d116e82c5a0f81ad3ed7dbb3532778060e52a08b
Masoud Sh <zzmasoud@gmail.com>
2022-09-12 10:56:43 +0200

    Improve `ZZHorizontalSelectorView` to conforms to `ZZHorizontalSelector_VD`

--- ZZNewTaskView/Models/External/ZZHorizontalSelectorView/View/ZZHorizontalSelectorView.swift
+++ ZZNewTaskView/Models/External/ZZHorizontalSelectorView/View/ZZHorizontalSelectorView.swift
@@ -6,17 +6,38 @@
 //
 
 import Foundation
+import AppKit
 
 protocol ZZHorizontalSelector_VD: AnyObject {
     func fillUI()
 }
 
 class ZZHorizontalSelectorView {
+    
+    @IBOutlet private weak var addButton: NSButton!
+    @IBOutlet private weak var collectionView: NSCollectionView!
+    
+    @objc private func addTapped() {
+        addNewItem?()
+    }
+    
     public var addNewItem: (()->Void)?
     
     public var viewModel: ZZHorizontalSelectorView_VMP! {
         didSet {
             viewModel.setView(delegate: self)
         }
+    }
+}
+
+extension ZZHorizontalSelectorView: ZZHorizontalSelector_VD {
+    func fillUI() {
+        addButton.isHidden = !viewModel.canAddNewItem
+        
+        // populate collectionView (datasouece + reload)
+        
+        viewModel.selectedIndexes.forEach({ _ in
+            // preselect selected items
+        })
     }
 }
