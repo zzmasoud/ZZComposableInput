7371db375391c78a2154d12897b241d6c2380634
zzmasoud <zzmasoud@gmail.com>
2023-02-01 20:59:38 +0100

    `send()` doesn't deliver data if text is empty

--- ZZTaskInput/ZZTaskInputTests/ZZTaskInputTests.swift
+++ ZZTaskInput/ZZTaskInputTests/ZZTaskInputTests.swift
@@ -7,22 +7,46 @@ import XCTest
 protocol ZZTaskInput {
     typealias Data = (title: String, description: String?)
     typealias SendCompletion = (Data) -> Void
-    func send(completion: SendCompletion)
+    var onSent: ZZTaskInput.SendCompletion { get }
+    func send()
 }
 
 class CLOCTaskInput {
     private(set) var text: String?
+    private(set) var onSent: ZZTaskInput.SendCompletion
+    
+    init(onSent: @escaping ZZTaskInput.SendCompletion) {
+        self.onSent = onSent
+    }
     
     func set(text: String?) {
         
     }
+    
+    func send() {}
 }
 
 class ZZTaskInputTests: XCTestCase {
     
     func test_init_textIsEmpty() {
-        let sut = CLOCTaskInput()
+        let sut = CLOCTaskInput(onSent: {_ in })
             
         XCTAssertNil(sut.text)
+    }
+    
+    func test_send_doesNotDeliverIfTextIsEmpty() {
+        let exp = expectation(description: "waiting for completion...")
+        exp.isInverted = true
+
+        let sut = CLOCTaskInput(
+            onSent: { _ in
+                XCTFail("expected to not get completion")
+                exp.fulfill()
+            }
+        )
+        
+        sut.send()
+        
+        wait(for: [exp], timeout: 1)
     }
 }
