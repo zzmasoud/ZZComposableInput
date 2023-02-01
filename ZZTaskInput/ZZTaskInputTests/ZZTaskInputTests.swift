5b236032cbf32779b5f2d42d4abb07f6cb66e8c8
zzmasoud <zzmasoud@gmail.com>
2023-02-01 21:15:57 +0100

    `send()` delivers proper title and description from parsing text

--- ZZTaskInput/ZZTaskInputTests/ZZTaskInputTests.swift
+++ ZZTaskInput/ZZTaskInputTests/ZZTaskInputTests.swift
@@ -24,8 +24,26 @@ class CLOCTaskInput {
     }
     
     func send() {
-        guard let _ = text else { return }
-        onSent(("", nil))
+        guard let text = text else { return }
+        
+        let data = parse(text: text)
+        onSent(data)
+    }
+    
+    private func parse(text: String) -> (title: String, description: String?) {
+        var result: (String, String?) = ("", nil)
+        
+        let substrings = text.split(separator: "\n", maxSplits: 1, omittingEmptySubsequences: true)
+        
+        if let title = substrings.first {
+            result.0 = String(title)
+        }
+        
+        if substrings.count > 1 , let desc = substrings.last {
+            result.1 = String(desc)
+        }
+        
+        return result
     }
 }
 
@@ -33,14 +51,14 @@ class ZZTaskInputTests: XCTestCase {
     
     func test_init_textIsEmpty() {
         let sut = CLOCTaskInput(onSent: {_ in })
-            
+        
         XCTAssertNil(sut.text)
     }
     
     func test_send_doesNotDeliverIfTextIsEmpty() {
         let exp = expectation(description: "waiting for completion...")
         exp.isInverted = true
-
+        
         let sut = CLOCTaskInput(
             onSent: { _ in
                 XCTFail("expected to not get completion")
@@ -55,13 +73,32 @@ class ZZTaskInputTests: XCTestCase {
     
     func test_send_deliversDataIfTextIsNotEmpty() {
         let exp = expectation(description: "waiting for completion...")
-
+        
         let sut = CLOCTaskInput(
             onSent: { _ in
                 exp.fulfill()
             }
         )
         sut.set(text: "Hello World!")
+        
+        sut.send()
+        
+        wait(for: [exp], timeout: 1)
+    }
+    
+    func test_send_deliversTitleAndDescriptionProperly() {
+        let exp = expectation(description: "waiting for completion...")
+        let title = "title"
+        let description = "desc"
+        
+        let sut = CLOCTaskInput(
+            onSent: { (receivedTitle, receivedDescription) in
+                XCTAssertEqual(title, receivedTitle)
+                XCTAssertEqual(description, receivedDescription)
+                exp.fulfill()
+            }
+        )
+        sut.set(text: [title, description].joined(separator: "\n"))
         
         sut.send()
         
