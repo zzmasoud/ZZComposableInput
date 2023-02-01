13eb336f728031a4ff112d788826d84c61013645
zzmasoud <zzmasoud@gmail.com>
2023-02-01 21:25:07 +0100

    Change `onSent` completion handler to not set on creation

--- ZZTaskInput/ZZTaskInputTests/ZZTaskInputTests.swift
+++ ZZTaskInput/ZZTaskInputTests/ZZTaskInputTests.swift
@@ -7,17 +7,14 @@ import XCTest
 protocol ZZTaskInput {
     typealias Data = (title: String, description: String?)
     typealias SendCompletion = (Data) -> Void
-    var onSent: ZZTaskInput.SendCompletion { get }
+    
+    var onSent: ZZTaskInput.SendCompletion? { get }
     func send()
 }
 
-class CLOCTaskInput {
+final class CLOCTaskInput: ZZTaskInput {
     private(set) var text: String?
-    private(set) var onSent: ZZTaskInput.SendCompletion
-    
-    init(onSent: @escaping ZZTaskInput.SendCompletion) {
-        self.onSent = onSent
-    }
+    var onSent: ZZTaskInput.SendCompletion?
     
     func set(text: String?) {
         self.text = text
@@ -27,7 +24,7 @@ class CLOCTaskInput {
         guard let text = text else { return }
         
         let data = parse(text: text)
-        onSent(data)
+        onSent?(data)
     }
     
     private func parse(text: String) -> (title: String, description: String?) {
@@ -50,21 +47,21 @@ class CLOCTaskInput {
 class ZZTaskInputTests: XCTestCase {
     
     func test_init_textIsEmpty() {
-        let sut = CLOCTaskInput(onSent: {_ in })
+        let sut = CLOCTaskInput()
         
         XCTAssertNil(sut.text)
     }
     
     func test_send_doesNotDeliverIfTextIsEmpty() {
+        let sut = CLOCTaskInput()
+        
         let exp = expectation(description: "waiting for completion...")
         exp.isInverted = true
         
-        let sut = CLOCTaskInput(
-            onSent: { _ in
-                XCTFail("expected to not get completion")
-                exp.fulfill()
-            }
-        )
+        sut.onSent = { _ in
+            XCTFail("expected to not get completion")
+            exp.fulfill()
+        }
         
         sut.send()
         
@@ -72,34 +69,35 @@ class ZZTaskInputTests: XCTestCase {
     }
     
     func test_send_deliversDataIfTextIsNotEmpty() {
+        let sut = CLOCTaskInput()
+
         let exp = expectation(description: "waiting for completion...")
         
-        let sut = CLOCTaskInput(
-            onSent: { _ in
-                exp.fulfill()
-            }
-        )
-        sut.set(text: "Hello World!")
+        sut.onSent = { _ in
+            exp.fulfill()
+        }
         
+        sut.set(text: "Hello World!")
         sut.send()
         
         wait(for: [exp], timeout: 1)
     }
     
     func test_send_deliversTitleAndDescriptionProperly() {
-        let exp = expectation(description: "waiting for completion...")
         let title = "title"
         let description = "desc"
         
-        let sut = CLOCTaskInput(
-            onSent: { (receivedTitle, receivedDescription) in
-                XCTAssertEqual(title, receivedTitle)
-                XCTAssertEqual(description, receivedDescription)
-                exp.fulfill()
-            }
-        )
-        sut.set(text: [title, description].joined(separator: "\n"))
+        let exp = expectation(description: "waiting for completion...")
+
+        let sut = CLOCTaskInput()
         
+        sut.onSent = { (received) in
+            XCTAssertEqual(title, received.title)
+            XCTAssertEqual(description, received.description)
+            exp.fulfill()
+        }
+        
+        sut.set(text: [title, description].joined(separator: "\n"))
         sut.send()
         
         wait(for: [exp], timeout: 1)
