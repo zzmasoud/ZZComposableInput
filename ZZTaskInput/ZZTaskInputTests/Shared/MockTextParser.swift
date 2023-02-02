0f4967719d8c73a087449f7b6ff5d5570664f7ca
zzmasoud <zzmasoud@gmail.com>
2023-02-02 14:35:54 +0100

    Move `MockTextParser` to shared files

---
+++ ZZTaskInput/ZZTaskInputTests/Shared/MockTextParser.swift
@@ -0,0 +1,20 @@
+//
+//  Copyright © zzmasoud (github.com/zzmasoud).
+//  
+
+import ZZTaskInput
+
+class MockTextParser: ZZTextParser {
+    typealias Parsed = (title: String, description: String?)
+    
+    var result = Parsed("", nil)
+    private(set) var separator: Character = "\n"
+    private var parseTextCalles = [String]()
+    
+    var parseTextCount: Int { parseTextCalles.count }
+    
+    func parse(text: String) -> (title: String, description: String?) {
+        parseTextCalles.append(text)
+        return result
+    }
+}
