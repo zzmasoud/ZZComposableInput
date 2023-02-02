7841ddda83326dae0088bf2be4bee24ff9465c0b
zzmasoud <zzmasoud@gmail.com>
2023-02-02 10:22:55 +0100

    Decouple parsing functionality into a new protocol `ZZTextParser` to add more flexibility and single responsibility

--- ZZTaskInput/ZZTaskInputTests/ZZTaskInputTests.swift
+++ ZZTaskInput/ZZTaskInputTests/ZZTaskInputTests.swift
@@ -4,30 +4,15 @@
 
 import XCTest
 
-protocol ZZTaskInput {
-    typealias Data = (title: String, description: String?)
-    typealias SendCompletion = (Data) -> Void
-    
-    var onSent: ZZTaskInput.SendCompletion? { get }
-    func send()
+protocol ZZTextParser {
+    associatedtype Parsed
+    func parse(text: String) -> Parsed
 }
 
-final class CLOCTaskInput: ZZTaskInput {
-    private(set) var text: String?
-    var onSent: ZZTaskInput.SendCompletion?
+final class CLOCTextParser: ZZTextParser {
+    typealias Parsed = (title: String, description: String?)
     
-    func set(text: String?) {
-        self.text = text
-    }
-    
-    func send() {
-        guard let text = text, !text.isEmpty else { return }
-        
-        let data = parse(text: text)
-        onSent?(data)
-    }
-    
-    private func parse(text: String) -> (title: String, description: String?) {
+    func parse(text: String) -> (title: String, description: String?) {
         var result: (String, String?) = ("", nil)
         
         let components = text.split(separator: "\n")
@@ -46,6 +31,35 @@ final class CLOCTaskInput: ZZTaskInput {
         }
         
         return result
+    }
+}
+
+protocol ZZTaskInput {
+    typealias Data = (title: String, description: String?)
+    typealias SendCompletion = (Data) -> Void
+    
+    var onSent: ZZTaskInput.SendCompletion? { get }
+    func send()
+}
+
+final class CLOCTaskInput<T: CLOCTextParser>: ZZTaskInput {
+    private let textParser: T
+    private(set) var text: String?
+    var onSent: ZZTaskInput.SendCompletion?
+    
+    init(textParser: T) {
+        self.textParser = textParser
+    }
+    
+    func set(text: String?) {
+        self.text = text
+    }
+    
+    func send() {
+        guard let text = text, !text.isEmpty else { return }
+        
+        let data = textParser.parse(text: text)
+        onSent?(data)
     }
 }
 
@@ -140,12 +154,13 @@ class ZZTaskInputTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> CLOCTaskInput {
-        let sut = CLOCTaskInput()
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> CLOCTaskInput<CLOCTextParser> {
+        let textParser = CLOCTextParser()
+        let sut = CLOCTaskInput(textParser: textParser)
         return sut
     }
     
-    private func expect(_ sut: CLOCTaskInput, toCompleteWith expected: (title: String, description: String?)?, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+    private func expect(_ sut: CLOCTaskInput<CLOCTextParser>, toCompleteWith expected: (title: String, description: String?)?, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
         let exp = expectation(description: "waiting for completion...")
         exp.isInverted = expected == nil
         
