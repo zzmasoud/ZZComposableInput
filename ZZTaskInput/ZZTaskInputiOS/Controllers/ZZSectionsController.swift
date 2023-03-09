23926ea079d0a10c646e5f421a761296e9dd2e2c
zzmasoud <zzmasoud@gmail.com>
2023-03-09 11:06:59 +0100

    Move the view model class to separated file and remove main module dependency from `ZZSectionsController`

--- ZZTaskInput/ZZTaskInputiOS/Controllers/ZZSectionsController.swift
+++ ZZTaskInput/ZZTaskInputiOS/Controllers/ZZSectionsController.swift
@@ -3,32 +3,6 @@
 //  
 
 import UIKit
-import ZZTaskInput
-
-final class ZZSectionsViewModel {
-    typealias IndexMapper = (Int) -> CLOCSelectableProperty
-    
-    private let loader: ZZItemsLoader
-    let sections: [String]
-    private let indexMapper: IndexMapper
-
-    init(loader: ZZItemsLoader, sections: [String], indexMapper: @escaping IndexMapper) {
-        self.loader = loader
-        self.sections = sections
-        self.indexMapper = indexMapper
-    }
-
-    var onLoading: ((Bool) -> Void)?
-    var onLoad: ((Int, CLOCItemsLoader.FetchItemsResult) -> Void)?
-    
-    func selectSection(index: Int) {
-        onLoading?(true)
-        loader.loadItems(for: index, completion: { [weak self] result in
-            self?.onLoading?(false)
-            self?.onLoad?(index, result)
-        })
-    }
-}
 
 final class ZZSectionsController: NSObject {
     private(set) lazy var view: UISegmentedControl = {
