929daf76c7cfb42b12d7ade6c5c24bcf88b39eab
zzmasoud <zzmasoud@gmail.com>
2023-03-09 11:05:43 +0100

    Move `ZZSectionsController` loading state management to `ZZSectionsViewModel` and now the previous class acts as a binder between the View and ViewModel

--- ZZTaskInput/ZZTaskInputiOS/Composers/ZZTaskInputViewUIComposer.swift
+++ ZZTaskInput/ZZTaskInputiOS/Composers/ZZTaskInputViewUIComposer.swift
@@ -15,48 +15,49 @@ public final class ZZTaskInputViewComposer {
         itemsLoader: ZZItemsLoader,
         preSelectedItemsHandler: PreSelectedItemsHandler?
     ) -> ZZTaskInputView {
-            let sectionsController = ZZSectionsController(
-                loader: itemsLoader,
-                sections: ["date", "time", "project", "weekdaysRepeat"],
-                indexMapper: { index in
-                    return CLOCSelectableProperty(rawValue: index)!
-                })
+        let sectionsViewModel = ZZSectionsViewModel(
+            loader: itemsLoader,
+            sections: ["date", "time", "project", "weekdaysRepeat"],
+            indexMapper: { index in
+                return CLOCSelectableProperty(rawValue: index)!
+            })
+        let sectionsController = ZZSectionsController(viewModel: sectionsViewModel)
         
-            let inputView = ZZTaskInputView(sectionsController: sectionsController)
-            inputView.onCompletion = { [weak inputView] in
-                guard let text = inputView?.text else { return }
-                let (title, description) = textParser.parse(text: text)
-            }
-            
-            sectionsController.onLoad = { [weak inputView] (index, result) in
-                guard let inputView = inputView else { return }
-                if let items = try? result.get() {
-                    let preSelectedItems = preSelectedItemsHandler?(index)
-                    let container = CLOCItemsContainer(
-                        items: items,
-                        preSelectedItems: preSelectedItems,
-                        selectionType: CLOCSelectableProperty(rawValue: index)!.selectionType)
-                    
-                    inputView.cellControllers = adaptContainerItemsToCellControllers(
-                        forwardingTo: inputView,
-                        container: container)
-                    
-                    inputView.onSelection = { [weak container] index in
-                        container?.select(at: index)
-                    }
-                    
-                    inputView.onDeselection = { [weak container] index in
-                        container?.unselect(at: index)
-                    }
-                    
-                } else {
-                    inputView.cellControllers = []
-                }
-            }
-
-            return inputView
+        let inputView = ZZTaskInputView(sectionsController: sectionsController)
+        inputView.onCompletion = { [weak inputView] in
+            guard let text = inputView?.text else { return }
+            let (title, description) = textParser.parse(text: text)
         }
         
+        sectionsViewModel.onLoad = { [weak inputView] (index, result) in
+            guard let inputView = inputView else { return }
+            if let items = try? result.get() {
+                let preSelectedItems = preSelectedItemsHandler?(index)
+                let container = CLOCItemsContainer(
+                    items: items,
+                    preSelectedItems: preSelectedItems,
+                    selectionType: CLOCSelectableProperty(rawValue: index)!.selectionType)
+                
+                inputView.cellControllers = adaptContainerItemsToCellControllers(
+                    forwardingTo: inputView,
+                    container: container)
+                
+                inputView.onSelection = { [weak container] index in
+                    container?.select(at: index)
+                }
+                
+                inputView.onDeselection = { [weak container] index in
+                    container?.unselect(at: index)
+                }
+                
+            } else {
+                inputView.cellControllers = []
+            }
+        }
+        
+        return inputView
+    }
+    
     private static func adaptContainerItemsToCellControllers(forwardingTo controller: ZZTaskInputView, container: CLOCItemsContainer) -> [ZZSelectableCellController] {
         (container.items ?? []).map { item in
             return ZZSelectableCellController(text: item.title, isSelected: {
--- ZZTaskInput/ZZTaskInputiOS/Controllers/ZZSectionsController.swift
+++ ZZTaskInput/ZZTaskInputiOS/Controllers/ZZSectionsController.swift
@@ -5,14 +5,36 @@
 import UIKit
 import ZZTaskInput
 
-final class ZZSectionsController: NSObject {
+final class ZZSectionsViewModel {
     typealias IndexMapper = (Int) -> CLOCSelectableProperty
     
+    private let loader: ZZItemsLoader
+    let sections: [String]
+    private let indexMapper: IndexMapper
+
+    init(loader: ZZItemsLoader, sections: [String], indexMapper: @escaping IndexMapper) {
+        self.loader = loader
+        self.sections = sections
+        self.indexMapper = indexMapper
+    }
+
+    var onLoading: ((Bool) -> Void)?
+    var onLoad: ((Int, CLOCItemsLoader.FetchItemsResult) -> Void)?
+    
+    func selectSection(index: Int) {
+        onLoading?(true)
+        loader.loadItems(for: index, completion: { [weak self] result in
+            self?.onLoading?(false)
+            self?.onLoad?(index, result)
+        })
+    }
+}
+
+final class ZZSectionsController: NSObject {
     private(set) lazy var view: UISegmentedControl = {
-        let segmentedControl = UISegmentedControl(items: sections)
-        segmentedControl.addTarget(self, action: #selector(selectSection), for: .valueChanged)
+        let segmentedControl = UISegmentedControl(items: viewModel.sections)
         segmentedControl.selectedSegmentIndex = -1
-        return segmentedControl
+        return bind(segmentedControl)
     }()
     
     private(set) lazy var label: UILabel = {
@@ -21,27 +43,22 @@ final class ZZSectionsController: NSObject {
         return label
     }()
     
-    private let loader: ZZItemsLoader
-    private let sections: [String]
-    private let indexMapper: IndexMapper
+    private let viewModel: ZZSectionsViewModel
+    var onLoading: (() -> Void)?
     
-    init(loader: ZZItemsLoader, sections: [String], indexMapper: @escaping IndexMapper) {
-        self.loader = loader
-        self.sections = sections
-        self.indexMapper = indexMapper
+    init(viewModel: ZZSectionsViewModel) {
+        self.viewModel = viewModel
     }
     
-    var onLoading: (() -> Void)?
-    var onLoad: ((Int, CLOCItemsLoader.FetchItemsResult) -> Void)?
+    private func bind(_ view: UISegmentedControl) -> UISegmentedControl {
+        view.addTarget(self, action: #selector(selectSection), for: .valueChanged)
+        viewModel.onLoading = { [weak self] isLoading in
+            self?.label.isHidden = isLoading
+        }
+        return view
+    }
     
     @objc private func selectSection() {
-        let index = view.selectedSegmentIndex
-//        let section = indexMapper(index)
-
-        label.isHidden = true
-        loader.loadItems(for: index, completion: { [weak self] result in
-            self?.label.isHidden = false
-            self?.onLoad?(index, result)
-        })
+        viewModel.selectSection(index: view.selectedSegmentIndex)
     }
 }
