38e1aa44bc3ad3f17056c305779efd886d9636c7
zzmasoud <zzmasoud@gmail.com>
2023-03-12 12:38:26 +0100

    Extract SUT creation to a factory method

--- ZZTaskInput/ZZTaskInputTests/Presentation/SectionsPresenterTests.swift
+++ ZZTaskInput/ZZTaskInputTests/Presentation/SectionsPresenterTests.swift
@@ -11,14 +11,22 @@ final class SectionsPresenter {
 class SectionsPresenterTests: XCTestCase {
     
     func test_init_doesntSendMessagesToView() {
-        let view = ViewSpy()
-        
-        _ = SectionsPresenter(view: view)
+        let (_, view) = makeSUT()
         
         XCTAssertTrue(view.messages.isEmpty)
     }
     
     // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: SectionsPresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = SectionsPresenter(view: view)
+        
+        trackForMemoryLeaks(view)
+        trackForMemoryLeaks(sut)
+
+        return (sut, view)
+    }
     
     private class ViewSpy {
         let messages = [Any]()
