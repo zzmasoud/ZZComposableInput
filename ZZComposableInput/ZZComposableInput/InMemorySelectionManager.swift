fe229613cebdb1155b7975d5354877654ebd66f8
zzmasoud <zzmasoud@gmail.com>
2023-05-08 10:03:34 +0200

    Move the final class to sources

---
+++ ZZComposableInput/ZZComposableInput/InMemorySelectionManager.swift
@@ -0,0 +1,34 @@
+//
+//  Copyright © zzmasoud (github.com/zzmasoud).
+//  
+
+import Foundation
+
+/*
+ Scenario:
+ 1. User selects 2 items for section 0
+ 2. User switches to section 1
+ 3. <ItemsLoader> loads items for section 1
+ 4. Mapping data to <ItemsContainer>
+ 5. User switches to section 0 again
+ 6. <ItemsLoader> loads items for section 0 OR load cached data
+ 7. Mapping data to <ItemsContainer> THIS ALWAYS HAPPENS IN THE CURRENT DESIGN
+  
+ So we might loose the user's previously selected items, this entity is here to prevent it
+ Even if <ItemsLoader> caches items, mapping data to <ItemsContainer> happens, so this is mandatory to keep user's selected items.
+ */
+
+public final class InMemorySelectionManager<Container: ItemsContainer> {
+    private(set) public var loadedContainers = [Int: Container]()
+    
+    public init() {}
+
+    public func sync(container: Container, forSection section: Int) {
+        loadedContainers[section]?.selectedItems?.forEach({ item in
+            if let index = container.items!.firstIndex(of: item) {
+                container.select(at: index)
+            }
+        })
+        loadedContainers[section] = container
+    }
+}
